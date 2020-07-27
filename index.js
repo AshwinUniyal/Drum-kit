f8537da470a31bd8e513bce6e2ecc15eafaba8be
Ashwin Kumar Uniyal <60223416+AshwinUniyal@users.noreply.github.com>
2020-07-27 14:21:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+//alert box
+
+var alerted = sessionStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+     alert("Haan maine hi banaya hai 😤, Dekhle ab Nimmi 💃 \n ~Ash ");
+     sessionStorage.setItem('alerted','yes');
+     }
+
+
+//DETECTING BUTTON PRESS
+
+var numberOfButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0; i<numberOfButtons; i++){
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+var buttonInnerHTML= this.innerHTML;
+
+makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+  });
+
+}
+
+
+//DETECTING KEYBOARD PRESS
+document.addEventListener("keydown", function(event){ //event is not predefined, type anything
+
+makeSound(event.key);
+buttonAnimation(event.key);                                //event.key gives which key was pressed
+
+ });
+
+function makeSound(key){
+  switch (key) {
+    // EITHER USE IF-ELSE OR SWITCH
+    // if(buttonInnerHTML=="w"){
+    //   var audio= new Audio("sounds/tom-1.mp3");
+    //   audio.play();
+    // }
+    case "w":
+    var tom1= new Audio("sounds/tom-1.mp3");
+    tom1.play();
+
+      break;
+
+    case "a":
+    var tom2= new Audio("sounds/tom-2.mp3");
+    tom2.play();
+
+      break;
+
+    case "s":
+    var tom3= new Audio("sounds/tom-3.mp3");
+    tom3.play();
+
+      break;
+
+    case "d":
+    var tom4= new Audio("sounds/tom-4.mp3");
+    tom4.play();
+
+      break;
+
+    case "j":
+    var snare= new Audio("sounds/snare.mp3");
+   snare.play();
+
+      break;
+
+    case "k":
+    var crash= new Audio("sounds/crash.mp3");
+    crash.play();
+    break;
+
+    case "l":
+    var kick= new Audio("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+
+
+    default: console.log(buttonInnerHTML);
+  }
+
+}
+function buttonAnimation(currentKey){
+var activeButton= document.querySelector("." + currentKey);
+activeButton.classList.add("pressed");
+setTimeout(function (){
+  activeButton.classList.remove("pressed");
+},100);
+
+}
